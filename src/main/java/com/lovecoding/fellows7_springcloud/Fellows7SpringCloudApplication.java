a68a04e218ab8f48f792f258b0800f1d46807dd0
nhj2001 <74459996+nhj2001@users.noreply.github.com>
2021-09-11 14:14:28 +0800

    Initial commit

---
+++ src/main/java/com/lovecoding/fellows7_springcloud/Fellows7SpringCloudApplication.java
@@ -0,0 +1,13 @@
+package com.lovecoding.fellows7_springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fellows7SpringCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Fellows7SpringCloudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lovecoding/fellows7_springcloud/Fellows7SpringCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lovecoding.fellows7_springcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fellows7SpringCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
